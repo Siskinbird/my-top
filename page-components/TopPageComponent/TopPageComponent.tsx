9928f0a35fbc9ca7ac839a5023a7980c7e34f433
Dmitrii Chizhov <dimachig@gmail.com>
2022-11-29 21:11:59 +0300

    feat: Top page component

---
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -0,0 +1,25 @@
+import { TopPageComponentProps} from './TopPageComponent.props';
+import styles from './TopPageComponent.module.css';
+import cn from 'classnames';
+import {Card, HhData, Htag, Tag} from "../../components";
+
+export const TopPageComponent = ({page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.title}>
+                <Htag tag='h1'>{page.title}</Htag>
+                {products && <Tag color='gray' size='m'>{products.length}</Tag>}
+                <span>Sort</span>
+            </div>
+            <div>
+                {products && products.map((p => (<div key={p._id}>{p.title}</div>)))}
+            </div>
+            <div className={styles.hhTitle}>
+                <Htag tag='h2'>Вакансии: {page.category}</Htag>
+                <Tag color='red' size='m'>hh.ru</Tag>
+            </div>
+            <HhData { ...page.hh }/>
+        </div>
+
+    );
+};
