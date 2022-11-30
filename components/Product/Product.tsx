cb732c0248e5b05e36f68bd6b4575815baf35838
Dmitrii Chizhov <dimachig@gmail.com>
2022-11-30 16:04:10 +0300

    add: Product component start layout

---
+++ components/Product/Product.tsx
@@ -0,0 +1,21 @@
+import { ProductProps } from './Product.props';
+import styles from './Product.module.css';
+import cn from 'classnames';
+import {Card} from "../Card/Card";
+import {Rating} from "../Rating/Rating";
+import {Tag} from "../Tag/Tag";
+
+export const Product = ({product, className, ...props }: ProductProps): JSX.Element => {
+    return (
+        <Card className={styles.product}>
+            <div className={styles.logo}><img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title}/></div>
+            <div className={styles.title}>{product.title}</div>
+            <div className={styles.price}>{product.price}</div>
+            <div className={styles.credit}>{product.credit}</div>
+            <div className={styles.prating}><Rating rating={product.reviewAvg ?? product.initialRating}/></div>
+            <div className={styles.tags}>цена</div>
+            <div className={styles.creditTitle}>кредит</div>
+            <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+        </Card>
+    );
+};
